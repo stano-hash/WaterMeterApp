c223bcd51fb7968a164717a7c38551ac4d2b4ac3
Your Name <your@email.com>
2026-06-22 14:02:31 +0300

    commit all

--- android/app/build/intermediates/cxx/Debug/2w1t1eg5/logs/arm64-v8a/prefab_command.bat
+++ android/app/build/intermediates/cxx/Debug/2w1t1eg5/logs/arm64-v8a/prefab_command.bat
@@ -16,7 +16,7 @@
   --ndk-version ^
   27 ^
   --output ^
-  "C:\\Users\\user\\AppData\\Local\\Temp\\agp-prefab-staging7099804242218697969\\staged-cli-output" ^
+  "C:\\Users\\user\\AppData\\Local\\Temp\\agp-prefab-staging15997873790724366941\\staged-cli-output" ^
   "C:\\Users\\user\\.gradle\\caches\\8.14\\transforms\\8a13960de8e6fe2f78c8c2d824562bb1\\transformed\\react-android-0.73.4-debug\\prefab" ^
   "C:\\Users\\user\\.gradle\\caches\\8.14\\transforms\\c1ef3763cf49bc85d8fff96bb6fe131d\\transformed\\fbjni-0.5.1\\prefab" ^
   "C:\\Users\\user\\.gradle\\caches\\8.14\\transforms\\6b9bebe1b5dc89cb90f7fae0a4b75204\\transformed\\hermes-android-0.73.4-debug\\prefab"
